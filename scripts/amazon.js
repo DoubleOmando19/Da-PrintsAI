097cc951a2f7b4bb261a21f4d639c3eb2dbd5961
HenryA <henry_alcaide@hotmail.com>
2025-05-22 14:35:26 -0400

    css adj

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -13,7 +13,7 @@ products.forEach((product) => {
         <div cursor: pointer; title="Copyright 2025 Alcaide" style="display: flex; font-family: copperplate; margin-top: 155px; margin-left: -40px; width: 20px; height: 10px;">C</div>
       </div>
 
-      <div style="verticial-align: top;" class="product-name limit-text-to-2-lines">
+      <div class="product-name limit-text-to-2-lines">
         ${product.name}
       </div>
 
